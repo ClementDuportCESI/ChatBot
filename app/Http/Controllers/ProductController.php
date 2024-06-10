55ebd6a5b83fb6675ba75987043a0e3e49e38e60
ClementDuportCESI <clement.duport@viacesi.fr>
2024-06-10 11:16:07 +0200

    feat : product controller

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\UpdateProductRequest;
+use App\Http\Requests\CreateProductRequest;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+
+    public function index()
+    {
+        $products = Product::paginate(10);
+        return view('product.index', ["products" => $products]);
+    }
+
+    public function search(Request $request)
+    {
+        $query = $request->input('query');
+
+        $products = Product::where('name', 'LIKE', "%{$query}%")
+            ->orderBy('created_at', 'desc')
+            ->paginate(10)
+            ->appends(['query' => $query]);
+
+        return view('product.search', compact('products', 'query'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('product.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(CreateProductRequest $request)
+    {
+        $products = Product::create(
+            $request->validated()
+        );
+
+        return redirect()->route("product.show", $products);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Product $product)
+    {
+        return view('product.show', compact("product"));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Product $product)
+    {
+        // dd($product);
+        return view('product.edit', compact("product"),);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateProductRequest $request, Product $product)
+    {
+
+        $product->update($request->validated());
+        return redirect()->route("product.show", $product);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Product $product)
+    {
+        $product->delete();
+        return redirect()->route("product.index");
+    }
+}
