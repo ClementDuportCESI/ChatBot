ffb93308ebd2bd1b0ee3b6528f6e6f85a9e4ae4f
ClementDuportCESI <clement.duport@viacesi.fr>
2024-06-10 11:16:45 +0200

    feat : product routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/produits', ProductController::class . "@index")->name("home.index");
+Route::get('/produits/{product}', ProductController::class . "@show")->name("product.show");
+
+Route::get('/admin/produits', ProductController::class . "@index")->name("product.index");
+Route::get('/admin/produits/modifier/{product}', ProductController::class . "@edit")->name("product.edit");
+Route::put('/admin/produits/modifier/{product}', ProductController::class . "@update")->name("product.update");
+Route::get('/admin/produits/ajouter', ProductController::class . "@create")->name("product.create");
+Route::post('/admin/produits/store', ProductController::class . "@store")->name("product.store");
+Route::delete('/admin/produits/{product}', ProductController::class . "@destroy")->name("product.destroy");
+Route::get('/admin/produits-recherche}', ProductController::class . "@search")->name("product.search");
+
